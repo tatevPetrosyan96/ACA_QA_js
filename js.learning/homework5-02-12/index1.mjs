bb7954a8a34ab7458e423a8139444f2640d56882
Tatev Petrosyan <tatew.petrosyan@gmail.com>
2024-12-03 11:25:33 +0400

    Find Common Characters Between Two Strings

---
+++ js.learning/homework5-02-12/index1.mjs
@@ -0,0 +1,17 @@
+
+import { STR1, STR2 } from './index2.mjs';
+let commonCount = 0; 
+
+
+for(let i in STR1){
+    let element = STR1[i];
+
+    for(let k in STR2){
+        if(element == STR2[k]){
+            commonCount++;
+            break;
+        }
+            
+    }   
+}
+console.log(commonCount);
